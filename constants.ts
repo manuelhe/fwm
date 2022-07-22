f9b5ce07308fc021042f74e4a4e000c55326a11d
Manuel Herrera <334509+manuelhe@users.noreply.github.com>
2022-07-22 18:07:38 -0500

    chore: fixed contants

--- constants.ts
+++ constants.ts
@@ -1,6 +1,6 @@
 export const Config = {
   SITE_NAME: 'Fundación Waldorf Montecervino',
   SITE_DESCRIPTION: 'Fundación Waldorf Montecervino: Origen, Propósito, Evolución y Retos',
-  SITE_BASE_URL: 'https://fundacionwaldorfmontecervino.org/',
-  DEFAULT_SOCIAL_IMAGE_URL: 'https://fundacionwaldorfmontecervino.org/assts/fav.png'
+  SITE_BASE_URL: 'https://fundacionwaldorfmontecervino.org',
+  DEFAULT_SOCIAL_IMAGE_URL: 'https://fundacionwaldorfmontecervino.org/assets/fav.png'
 };
